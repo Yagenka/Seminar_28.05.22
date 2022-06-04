a4171c2f146ccded666f4b6ab1670c2dd8bba215
Tamara <tomritol@gmail.com>
2022-06-04 10:13:38 +0300

    add task 43

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -3,9 +3,9 @@
 // x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
 // пользователем.
 
-
 Console.WriteLine("Введите значение b1");
 int b1 = int.Parse(Console.ReadLine());
+
 
 Console.WriteLine("Введите значение k1");
 int k1 = int.Parse(Console.ReadLine());
@@ -17,23 +17,17 @@ Console.WriteLine("Введите значение k2");
 int k2 = int.Parse(Console.ReadLine());
 
 
-void Method(int b11, int k11, int b22, int k22)
+void PointIntersection(int b11, int k11, int b22, int k22)
 {
-   
-        double temp = k11 - k22;
-        if (temp != 0)
-        {
-            double x = (b22 - b11) / temp;
+    double temp = k11 - k22;
+    if (temp != 0)
+    {
+        double x = (b22 - b11) / temp;
         double y = k11 * x + b11;
-
-        Console.WriteLine(x);
-        Console.WriteLine(y);
-
         Console.WriteLine($"координаты точки пересечения для двух прямых ({x};{y})");
         }
-        else if (k22==k11) Console.WriteLine("Отрезки параллельны");
-    
+    else if (k22 == k11) Console.WriteLine("Прямые параллельны друг другу");
 }
 
-Method(b1, k1, b2, k2);
+PointIntersection(b1, k1, b2, k2);
 
